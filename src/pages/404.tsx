87500d3f96eb734b50c6ab3eee905de5647c02a6
diptasahacse <diptasaha.lpu.cse@gmail.com>
2023-08-11 22:10:22 +0600

    --

---
+++ src/pages/404.tsx
@@ -0,0 +1,8 @@
+import DashboardLayout from "@/components/Layouts/DashboardLayout";
+import React from "react";
+
+const NotFound = () => {
+  return <DashboardLayout>Not Found</DashboardLayout>;
+};
+
+export default NotFound;
